c3f9d863f7d90c4e59d1762b29d9e7877612b9ed
duyhung0311 <56124429+duyhung0311@users.noreply.github.com>
2021-03-30 20:06:53 +0700

    Create Register and Forget Password UI Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import Login from './feature/login/index'
+// import Login from './feature/login/index'
+import Register  from './feature/register'
 function App() {
   return (
     <div className="App">
-      <Login/>
+      {/* <Login/> */}
+<Register/>
     </div>
   );
 }
---
+++ frontend/src/feature/register/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './style.css'
+function register() {
+    return (
+        <div>
+            aa
+        </div>
+    )
+}
+
+export default register

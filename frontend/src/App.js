7fa8f0717d279eb007630b97c50b594bc42ea37c
duyhung0311 <56124429+duyhung0311@users.noreply.github.com>
2021-03-31 16:08:00 +0700

    Create Homepage UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,15 @@ import logo from "./logo.svg";
 import "./App.css";
 // import Login from './feature/login/index'
 // import Register  from './feature/register'
-import Forgotpassword from "./feature/forgotpassword";
+// import Forgotpassword from "./feature/forgotpassword";
+import Home from './feature/homepage'
 function App() {
   return (
     <div className="App">
       {/* <Login/> */}
       {/* <Register/> */}
       <Forgotpassword/>
+      <Home/>
     </div>
   );
 }
---
+++ frontend/src/feature/homepage/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './style.css'
+function Home() {
+    return (
+        <div>
+            aa
+        </div>
+    )
+}
+
+export default Home

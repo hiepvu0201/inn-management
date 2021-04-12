8f9a2b3e44beb21b1a9b772c3db752fbdf4f07e0
duyhung0311 <56124429+duyhung0311@users.noreply.github.com>
2021-04-12 11:59:43 +0700

    Create Page Brand of Admin Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,9 @@ import "./App.css";
 // import Login from './feature/login/index'
 // import Register  from './feature/register'
 // import Forgotpassword from "./feature/forgotpassword";
-import Home from './feature/homepage'
-// import Component_NgangXanh from './components/component_ngangxanh'
-// import Component_DocXanh from '../src/components/component_docxanh'
-// import Component_NgangHong from './components/component_nganghong'
-// import Component_Ngang from './components/component_ngang'
-// import Component_NgangTrangHong from './components/component_ngangtranghong'
-// import Component_Block from './components/component_block'
-// import Component_Rectangle from './components/component_rectangle'
+// import Home from './feature/homepage'
+import Brand from './../src/feature/admin/brand'
+
 import Component_Block_Last from './components/component_block_last'
 function App() {
   return (
@@ -18,15 +13,8 @@ function App() {
       {/* <Login/> */}
       {/* <Register/> */}
       {/* <Forgotpassword/> */}
-      <Home/>
-      {/* <Component_NgangXanh/> */}
-      {/* <Component_DocXanh/> */}
-      {/* <Component_NgangHong/> */}
-      {/* <Component_Ngang/> */}
-      {/* <Component_NgangTrangHong/> */}
-      {/* <Component_Block/> */}
-      {/* <Component_Rectangle/> */}
-      {/* <Component_Block_Last/> */}
+      {/* <Home/> */}
+      <Brand/>
     </div>
   );
 }
---
+++ frontend/src/feature/admin/brand/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './style.css'
+function Brand() {
+    return (
+        <div>
+            11
+        </div>
+    )
+}
+
+export default Brand

538036a4cbaaafe39835808aa6e1277d43cea61a
duyhung0311 <56124429+duyhung0311@users.noreply.github.com>
2021-04-14 12:38:30 +0700

    Commit create page of managing customer of admin

---
+++ frontend/src/feature/admin/customer/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './style.css'
+import Menu_AdminPage from './../../../components/menu_adminpage'
+function Customer() {
+    return (
+        <div>
+            <div style={{width:"100%",height:"100vmax",backgroundColor:"#efefef"}}>
+                <div style={{height:"100px"}}>
+                    <Menu_AdminPage/>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Customer

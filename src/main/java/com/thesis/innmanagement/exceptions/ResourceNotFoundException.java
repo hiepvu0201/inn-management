50d7c8c1e17dc4ae67eb75d5d4384d039519304a
HoangHiep <hoanghiep@HoangHiepnoMacBook-puro.local>
2021-04-23 00:10:57 +0700

    remove none-models

--- src/main/java/com/thesis/innmanagement/exceptions/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.thesis.innmanagement.exceptions;
-
-public class ResourceNotFoundException extends Exception {
-    public ResourceNotFoundException(String message){
-        super(message);
-    }
-}

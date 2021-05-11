005d56d9b1f97638118c24ce94a3738b006c05a5
duyhung0311 <56124429+duyhung0311@users.noreply.github.com>
2021-05-11 12:36:38 +0700

    no message

---
+++ frontend/src/components/room_tag/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Room_tag() {
+    return (
+        <div>
+            <div>1111</div>
+        </div>
+    )
+}
+
+export default Room_tag
---
+++ frontend/src/feature/client/room/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './style.css'
+import Room_tag from './../../../components/room_tag'
+function Room() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Room

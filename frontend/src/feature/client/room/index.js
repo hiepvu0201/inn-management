b3ba869caaac9588c32e08c90fd9502eed5e0041
duyhung0311 <56124429+duyhung0311@users.noreply.github.com>
2021-05-11 12:40:40 +0700

    no message

--- frontend/src/feature/client/room/index.js
+++ frontend/src/feature/client/room/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import './style.css'
 import Room_tag from './../../../components/room_tag'
+import Menu_client from './../../../components/menu_client'
 function Room() {
     return (
         <div>
+            <div style={{height:"120px"}}></div>
             
         </div>
     )

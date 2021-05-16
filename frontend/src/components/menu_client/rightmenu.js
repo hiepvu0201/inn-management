7c3d2fcfe6781f48d3b2295ccee38f3c00fda714
duyhung0311 <56124429+duyhung0311@users.noreply.github.com>
2021-05-16 15:43:56 +0700

    create ui and call api of page notification - Client

---
+++ frontend/src/components/menu_client/rightmenu.js
@@ -0,0 +1,51 @@
+// import React from 'react';
+// import { Menu, Grid } from 'antd';
+// const SubMenu = Menu.SubMenu;
+// const MenuItemGroup = Menu.ItemGroup;
+
+// const { useBreakpoint } = Grid;
+
+// const RightMenu = () => {
+//   const { md } = useBreakpoint();
+//   return (
+//     <Menu mode={md ? "horizontal" : "inline"}>
+//       <Menu.Item key="mail">
+//         <a style={{fontFamily:'Kaushan Script, cursive'}} href="">Signin</a>
+//       </Menu.Item>
+//       <Menu.Item key="app">
+//         <a style={{fontFamily:'Kaushan Script, cursive'}} href="">Signup</a>
+//       </Menu.Item>
+//     </Menu>
+//   );
+// }
+
+// export default RightMenu;
+
+import React from 'react';
+import { Menu, Grid, Badge } from 'antd';
+import { ShoppingCartOutlined } from "@ant-design/icons";
+const SubMenu = Menu.SubMenu;
+const MenuItemGroup = Menu.ItemGroup;
+
+const { useBreakpoint } = Grid;
+
+const RightMenu = () => {
+  const { md } = useBreakpoint();
+  return (
+    <Menu mode={md ? "horizontal" : "inline"}>
+      <Menu.Item key="mail">
+        <a style={{ color: 'Black', fontFamily: 'Kaushan Script, cursive' }} href="">Signin</a>
+      </Menu.Item>
+      <Menu.Item key="app">
+        <a style={{ color: 'Black', fontFamily: 'Kaushan Script, cursive' }} href="">
+          
+            <Badge style={{fontFamily: "Noto Sans,sans-serif"}} count={5} >
+            <ShoppingCartOutlined/>
+            </Badge>
+        </a>
+      </Menu.Item>
+    </Menu>
+  );
+}
+
+export default RightMenu;

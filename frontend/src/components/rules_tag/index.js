fdfff066bee7f2395a6e4a41a11eac697a7415b6
duyhung0311 <56124429+duyhung0311@users.noreply.github.com>
2021-05-19 15:00:13 +0700

    [FE] page show rules

---
+++ frontend/src/components/rules_tag/index.js
@@ -0,0 +1,69 @@
+import React from "react";
+
+function Rules_tag(props) {
+  return (
+    <div>
+      <div
+        style={{
+          width: "100%",
+          height: "auto",
+          display: "block",
+          backgroundColor: "white",
+          boxShadow: "2px 5px gray",
+        }}
+      >
+        <div style={{ width: "100%", display: "flex" }}>
+          <div
+            style={{
+              width: "9%",
+              fontFamily: "PT Sans, sans-serif",
+              fontWeight: "bold",
+              fontSize: "15px",
+              textAlign: "left",
+            }}
+          >
+            Tên nội quy:
+          </div>
+          <div
+            style={{
+              width: "91%",
+              fontFamily: "PT Sans, sans-serif",
+              fontSize: "15px",
+              textAlign: "left",
+            }}
+          >
+            {/* Phòng trừ Covid */}
+            {props.name}
+          </div>
+        </div>
+        <div style={{ width: "100%", display: "flex" }}>
+          <div
+            style={{
+              width: "10%",
+              fontFamily: "PT Sans, sans-serif",
+              fontWeight: "bold",
+              fontSize: "15px",
+              textAlign: "left",
+            }}
+          >
+            Mô tả chi tiết:
+          </div>
+          <div
+            style={{
+              width: "90%",
+              fontFamily: "PT Sans, sans-serif",
+              fontSize: "15px",
+              textAlign: "left",
+              paddingRight: "15px",
+            }}
+          >
+            {/* Phòng trừ Covid */}
+            {props.description}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Rules_tag;

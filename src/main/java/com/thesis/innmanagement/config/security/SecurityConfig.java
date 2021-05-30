4d273062b5c3b273a18c79de9bccb0025f787a29
hiep.vu.0201@gmail.com <hiep.vu.0201@gmail.com>
2021-05-30 14:20:24 +0700

    permit create user

--- src/main/java/com/thesis/innmanagement/config/security/SecurityConfig.java
+++ src/main/java/com/thesis/innmanagement/config/security/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests().antMatchers("/api/v1/users/login")
+        http.csrf().disable().authorizeRequests().antMatchers("/api/v1/users/login", "/api/v1/users/")
                 .permitAll().anyRequest().authenticated()
                 .and().exceptionHandling().and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
